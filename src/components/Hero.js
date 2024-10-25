145b188326279f33d65c8697b1cec9765f396f93
saraNasser4 <saransser6@gmail.com>
2024-10-25 09:56:41 +0300

    add styling to Hero and Layout components

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,28 @@
+import { FaCheck, FaExclamationCircle } from "react-icons/fa";
 function Hero (){
+  const paragraphs = ['Tracking every coffee', 'Measuring your blood caffeine levels', 'Costing and quanitifying your addition']
+
     return (
-      <div>Hero</div>
+      <div className="mt-32 p-6">
+        <h1 className="font-bold text-4xl md:text-5xl">Coffee Tracking for Coffee <span className="border-b-4 border-dotted">Fiends!</span></h1>
+        <h3 className="text-2xl md:text-3xl my-6 md:my-8 font-semibold">Try <span className="font-bold"><span className="bg-amber-900 pl-3 pr-1 text-white dark:text-black">C</span>affiend</span> and start...</h3>
+        {paragraphs.map((text, index)=> {
+          return(
+            <div key={index} className="flex items-center gap-3 md:text-xl mb-2">
+              <FaCheck />
+              <p>{text}</p>
+            </div>
+          )
+        })}
+        <div className="bg-amber-100 dark:bg-amber-300 dark:text-black p-4 rounded-md max-w-[650px] my-6">
+          <div className="flex items-center gap-2 text-xl md:text-2xl font-bold">
+            <FaExclamationCircle /> 
+            <h4>Did you know...</h4>
+          </div>
+          <h5 className="text-[17px] md:text-xl my-4 font-bold">That caffeine's half-life is about 5 hours?</h5>
+          <p className="md:text-lg font-medium">This means that after 5 hours, half the caffeine you consumed is still in your system, keeping you alert longer! so if you drink a cup of coffee with 200mg of caffeine, 5 hours, later, you'll still have about 100 mg of caffeine in your system.</p>
+        </div>
+      </div>
     )
   }
   
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,7 +4,7 @@ import { CiCoffeeBean } from "react-icons/ci";
 function Layout (props){
   
   const Header = (
-    <header className="flex items-center justify-between max-w-[1260px] p-4 mx-auto dark:text-white h-20">
+    <header className="z-50 fixed top-0 left-0 right-0 max-w-[1220px] mx-auto flex items-center justify-between p-4 h-20 bg-white dark:bg-black">
       <div>
         <h2 className="font-bold text-3xl md:text-4xl md:mt-4"><span className="bg-amber-900 pl-3 pr-1 text-white dark:text-black">C</span>affiend</h2>
         <p className="md:mt-1 md:text-lg">For coffee insatiates</p>
@@ -18,16 +18,22 @@ function Layout (props){
   )
 
   const Footer = (
-    <footer></footer>
+    <footer className="mt-10 mb-5 text-center">
+      <p>
+        <span className="font-bold text-xl md:text-2xl mr-2"><span className="bg-amber-900 pl-3 pr-1 text-white dark:text-black">C</span>affiend</span>
+        was made by <a target="_blank" href="https://github.com/saraNasser4" rel="noreferrer" className="text-amber-900 text-xl md:text-2xl font-medium underline">Sara</a> using TailwindCSS and React JS
+      </p>
+
+    </footer>
   )
   return (
-    <>
+    <div className="max-w-[1260px] mx-auto dark:text-white">
       {Header}
       <main>
         {props.children}
       </main>
       {Footer}
-    </>
+    </div>
   )
 }
 
